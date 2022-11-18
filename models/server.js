4b4e44da84cfb4a78ba75ed6362ee742a5008eb8
Alan Jimenez <alan.danjr@gmail.com>
2022-11-17 20:34:16 -0600

    Backend sockets

---
+++ models/server.js
@@ -0,0 +1,47 @@
+// Servidor de express
+const express   = require('express');
+const http      = require('http');
+const socketio  = require('socket.io');
+const path      = require('path');
+
+const Sockets = require('./sockets');
+
+class Server {
+
+    constructor() {
+
+        this.app = express();
+        this.port = process.env.PORT;
+
+        // Http server
+        this.server = http.createServer( this.app );
+
+        // Sockets setup 
+        // Configuración del socket server
+        this.io = socketio( this.server, { /* Configuraciones */ } );
+    }
+
+    middlewares() {
+        // Desplegar el directorio público 
+        this.app.use( express.static( path.resolve( __dirname, "../public") ) );
+    }
+
+    configurateSockets() {
+        new Sockets( this.io );
+    }
+
+    execute() {
+        // Inicializar middlewares
+        this.middlewares();
+
+        // Inicializar sockets
+        this.configurateSockets();
+
+        // Inicializar server
+        this.server.listen( this.port, () => {
+            console.log('Server running at port ', this.port);
+        });
+    }
+}
+
+module.exports = Server;
